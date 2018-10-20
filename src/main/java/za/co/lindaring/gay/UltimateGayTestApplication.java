e966b58d84cf84b651892e519097c7d8803776ee
Lindaring <lndsimelane@gmail.com>
2018-10-20 04:37:12 +0200

    Init project

---
+++ src/main/java/za/co/lindaring/gay/UltimateGayTestApplication.java
@@ -0,0 +1,12 @@
+package za.co.lindaring.gay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateGayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateGayTestApplication.class, args);
+	}
+}
---
+++ src/test/java/za/co/lindaring/gay/UltimateGayTestApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.lindaring.gay;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UltimateGayTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
